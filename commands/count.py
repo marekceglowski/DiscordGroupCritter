eda229a7998f6795468ece0186669eb49f960302
tim-zones <78053973+tim-zones@users.noreply.github.com>
2021-01-27 03:45:03 -0500

    Update count.py
    updated to fit mongoDB structure

--- commands/count.py
+++ commands/count.py
@@ -1,15 +1,15 @@
-import discord
 from discord.ext import commands
-
+from services.db_service import db as _db
 
 class Count(commands.Cog):
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, lmy_bot):
+        self.bot = lmy_bot
 
     @commands.command(
         name="count", help="Displays the number of submissions in the queue"
     )
     async def count_crits(self, ctx):
+
         await ctx.send(
-            "Number of items in the queue is " + str(len(submission_list)) + "."
+            "Number of items in the queue is " + str(_db.submissions.count({"status" : "pending"})) + "."
         )
