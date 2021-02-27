06b249b98a6b636c0073029a27655de5c0590056
William Jackson <will-jj@users.noreply.github.com>
2021-02-27 20:53:01 +0000

    Add next command
    Add command to get the next submission from the queue, DM the user a link and set the status to complete.
    
    Closes #22

--- cogs/admin.py
+++ cogs/admin.py
@@ -8,6 +8,21 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name="next", help="Return next submission from queue")
+    @commands.has_permissions(administrator=True)
+    async def next(self, ctx):
+
+        submission_list = _db.get_ordered_queue_submissions()
+
+        if submission_list is None:
+            await ctx.author.send("You have reviewed all avialable submissions")
+            return
+
+        _db.set_submission_complete(submission_list[0])
+
+        text = "Submission to Review:\n" + "Link: " + submission_list[0].jump_url + "\n"
+        await ctx.author.send(text)
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
