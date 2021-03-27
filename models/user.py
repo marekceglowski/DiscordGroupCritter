f4677f986bb7183fb70862c17f0881aea2532d2d
William Jackson <will-jj@users.noreply.github.com>
2021-03-27 18:18:56 +0000

    DM the user on feedback (db changes) add the field dm_on_feedback to user add an option for the user to toggle this field if the user dm_on_feedback parameter is true they will receive a dm with a link to the feedback they received and the submission it reviews.

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 class User:
-    def __init__(self, user_id, rank_id):
+    def __init__(self, user_id, rank_id, dm_on_feedback=True):
         self.user_id = user_id  # PK discord's user id
         self.rank_id = rank_id  # FK ranks.id
+        self.dm_on_feedback = dm_on_feedback
